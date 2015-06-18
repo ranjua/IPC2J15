2fb7944607518aba87e70888c1a9ed6be2b8dd4e
ranjua <randy_juarez@hotmail.com>
2015-06-18 00:38:27 -0600

    f2

---
+++ FASE2/ProyectoIPC2/ProyectoIPC2/Clases/Verificar_usuario.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoIPC2.Clases
+{
+    public class Verificar_usuario
+    {
+
+        //Verifica el usuario y registra el Login
+        public bool Verificacion(string usuario, string email, string password)
+        {
+            bool Sesion_aceptada = false;
+            Base_de_Datos registro = new Base_de_Datos();
+            DataTable tabla = new DataTable();
+            try
+            {
+                //Verificacion de usuarios-contraseña
+                tabla = registro.FillTableData("Select password from quinielas.dbo.usuarios where usuario = '" + usuario + "' or email= '" + email + "';");
+                if (tabla.Rows.Count == 1)
+                {
+                    Cifrado Contraseña = new Cifrado();
+                    string ArrarB = Contraseña.StringB(Contraseña.Encriptar(password, "Webapptyson11"));
+                    DataRow rtabla = tabla.Rows[0];
+                    string ArrayA = rtabla[0].ToString();
+                    Sesion_aceptada = Contraseña.CompareByteArray(ArrayA, ArrarB);
+                }
+
+            }
+            catch
+            {
+            }
+            if (Sesion_aceptada == true)
+            {
+
+                tabla = registro.FillTableData("Select cod_usuario, usuario, nombre, apellido, cod_rol, email, estado, bloqueado" +
+                " from quinielas.dbo.usuarios where usuario = '" + usuario + "' or email= '" + email + "';");
+                foreach (DataRow drtabla in tabla.Rows)
+                {
+                    HttpContext.Current.Session["Cod_Usuario"] = drtabla[0].ToString();
+                    HttpContext.Current.Session["Usuario"] = drtabla[1].ToString();
+                    HttpContext.Current.Session["Cod_Rol_Usuario"] = drtabla[4].ToString();
+                    HttpContext.Current.Session["Nombre"] = drtabla[2].ToString() + " " + drtabla[3].ToString();
+                    HttpContext.Current.Session["Email"] = drtabla[5].ToString();
+
+                    //Actualizacion
+                    /*HttpContext.Current.Session["Actualizacion"] = "0";
+                    tabla = registro.FillTableData("Select cod_usuario from quinielas.dbo.Actualizacionusuario where cod_act= 1 and cod_usuario=" + HttpContext.Current.Session["Cod_Usuario"]);
+                    foreach (DataRow drtablaa in tabla.Rows)
+                    {
+                        //Actualización
+                        HttpContext.Current.Session["Actualizacion"] = "1";
+                    }*/
+
+                }
+
+                registro.Upd_New_DelUnValorQry("Insert into quinielas.dbo.ControlUsuarios (cod_Usuario, estatus, fechahora, IPCliente) values( " +
+                    HttpContext.Current.Session["Cod_Usuario"] + ", 'Activo', '" + DateTime.Now.ToString() + "', '" + HttpContext.Current.Request.UserHostAddress + "' )");
+            }
+
+            return Sesion_aceptada;
+        }
+        //Cookies
+        public void ObtenerDatos(int cod)
+        {
+            Base_de_Datos registro = new Base_de_Datos();
+            DataTable tabla = new DataTable();
+            tabla = registro.FillTableData("Select cod_usuario, usuario, nombre, apellido, cod_rol, email, estado, bloqueado" +
+            " from quinielas.dbo.usuarios where cod_usuario=" + cod);
+            foreach (DataRow drtabla in tabla.Rows)
+            {
+                HttpContext.Current.Session["Cod_Usuario"] = drtabla[0].ToString();
+                HttpContext.Current.Session["Usuario"] = drtabla[1].ToString();
+                HttpContext.Current.Session["Cod_Rol_Usuario"] = drtabla[4].ToString();
+                HttpContext.Current.Session["Nombre"] = drtabla[2].ToString() + " " + drtabla[3].ToString();
+                HttpContext.Current.Session["Email"] = drtabla[5].ToString();
+            }
+
+            registro.Upd_New_DelUnValorQry("Insert into quinielas.dbo.ControlUsuarios (cod_Usuario, estatus, fechahora, IPCliente) values( " +
+                    HttpContext.Current.Session["Cod_Usuario"] + ", 'Activo', '" + DateTime.Now.ToString() + "', '" + HttpContext.Current.Request.UserHostAddress + "' )");
+        }
+        //Obtener nombre Quiniela
+        public void ObtenerNQuiniela(int cod)
+        {
+            Base_de_Datos registro = new Base_de_Datos();
+            DataTable tabla = new DataTable();
+            tabla = registro.FillTableData("Select nombre from quinielas.dbo.quinielas where cod_quiniela=" + cod);
+            foreach (DataRow drtabla in tabla.Rows)
+            {
+                HttpContext.Current.Session["NQuiniela"] = drtabla[0].ToString();
+            }
+        }
+        //Crea password con el usuario
+        public string CreaPassword(string nombre)
+        {
+            string clave = "";
+            nombre = nombre.Replace(" ", String.Empty);
+            Random ran = new Random();
+
+            for (int x = 0; x < nombre.Length; x++)
+            {
+                clave = clave + nombre.Substring(x, 1) + ran.Next(10); ;
+            }
+            if (clave.Length > 6)
+            {
+                clave = clave.Substring(0, 5);
+            }
+            return clave;
+        }
+        //Crea Usuario aleatorio
+        public string CreaUsuario()
+        {
+            string Usuario = "";
+            Base_de_Datos busca = new Base_de_Datos();
+            DataTable tabla = new DataTable();
+            Random ran = new Random();
+            //Crea usuario
+            Usuario = ran.Next(100000) + "UsuarioSYS" + ran.Next(200000);
+            //Busca si ya existe
+            tabla = busca.FillTableData("Select cod_usuario from quinielas.dbo.usuarios where usuario = '" + Usuario + "'");
+            //Si Existe Crea usuario de nuevo
+            if (tabla.Rows.Count >= 1)
+            {
+                CreaUsuario();
+            }
+            return Usuario;
+        }
+
+        //Registra el Logout del usuario
+        public void Logout()
+        {
+            Base_de_Datos registro = new Base_de_Datos();
+            registro.Upd_New_DelUnValorQry("Insert into quinielas.dbo.ControlUsuarios (cod_Usuario, estatus, fechahora, IPCliente) values( " +
+                   HttpContext.Current.Session["Cod_Usuario"] + ", 'Inactivo', '" + DateTime.Now.ToString() + "', '" + HttpContext.Current.Request.UserHostAddress + "')");
+
+            HttpContext.Current.Session["Cod_Usuario"] = null;
+            HttpContext.Current.Session["Usuario"] = null;
+            HttpContext.Current.Session["Cod_Rol_Usuario"] = null;
+            HttpContext.Current.Session["Quiniela"] = null;
+            HttpContext.Current.Session["Nombre"] = null;
+        }
+
+
+    }
+}
---
+++ FASE2/ProyectoIPC2/ProyectoIPC2/Login.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ProyectoIPC2.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="frm_Login" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
