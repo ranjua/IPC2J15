fb3d25c2dc69963c31073621c7ea25b40486e869
ranjua <randy_juarez@hotmail.com>
2015-06-23 01:54:08 -0600

    f

---
+++ FASE2/ProyectoIPC2/ProyectoIPC2/Empleados/Estado_Lote.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="Estado_Lote.aspx.cs" Inherits="ProyectoIPC2.Empleados.Estado_Lote" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table style="width:100%;">
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <asp:DropDownList ID="Ddl_lote" runat="server">
+                </asp:DropDownList>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <asp:Button ID="Button1" runat="server" Text="Siguiente Estado" OnClick="Button1_Click" />
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ FASE2/ProyectoIPC2/ProyectoIPC2/Empleados/Estado_Lote.aspx.cs
@@ -0,0 +1,54 @@
+﻿using ProyectoIPC2.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoIPC2.Empleados
+{
+    public partial class Estado_Lote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LLenar();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LLenar()
+        {
+            Base_de_Datos base_de_datos = new Base_de_Datos();
+            DataTable tabla = new DataTable();
+            tabla = base_de_datos.FillTableData("Select cod_lote, fecha_salida, cod_sucursal from ProyectoIPC2.dbo.Lotes");
+            int x = 1;
+            Ddl_lote.Items.Clear();
+            try
+            {
+                foreach (DataRow drtabla in tabla.Rows)
+                {
+                    System.Web.UI.WebControls.ListItem Newitem = new System.Web.UI.WebControls.ListItem();
+                    Newitem.Value = drtabla[0].ToString();
+                    Newitem.Text = drtabla[2].ToString() + "" + drtabla[1].ToString();
+                    if (x == 1)
+                    {
+                        Newitem.Selected = true;
+                        x++;
+                    }
+                    Ddl_lote.Items.Add(Newitem);
+                    Ddl_lote.Visible = true;
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
